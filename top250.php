5c731d6f33b7e8bb6e7cf343c4c163104806dc19
luck <lucklgh@126.com>
2017-09-18 21:11:02 +0800

    文件写入，去除html标签失败

--- top250.php
+++ top250.php
@@ -17,6 +17,8 @@ function show_list()    //爬取一页暂停10s,可加入文件写入
     $i = 0;
     $rank = 1;
     $url = 'https://movie.douban.com/top250';
+    $list = fopen("top_list.txt","w+");
+    echo "file open...<br>";
     while($i < 10)
     {
         $html = get_html($url);
@@ -28,31 +30,51 @@ function show_list()    //爬取一页暂停10s,可加入文件写入
         $names = $douban->find('span.title,span.other'); //只爬取主名
 
         $reg = '/&nbsp;\/.*/';//html中&nbsp表示空格
+
         foreach ($names as $key => $value)
         {
             if (!preg_match($reg,$value))
             {
                echo "<br>".$rank++." ";
+               fwrite($list,"\n".(string)$rank." ");
             }
             echo "$value";
+            $text = strip_tags($value);
+            fwrite($list,$text);
         }
 
         //暂停爬取10s
-        sleep(10);
+        //sleep(10);
         //url生成
         $i++;
         $url = 'https://movie.douban.com/top250?start='.(string)($i*25).'&filter=';
-
+        //清空dom内存
+        $douban->clear();
     }
+    fclose($list);
 }
 show_list();
 
 function show_detail()
 {
+    $url = 'https://movie.douban.com/top250';
+
+    $html = get_html($url);
+
+    $douban = new simple_html_dom(); //创建simple_html_dom对象douban
+
+    $douban->load($html);
+
+    $links = $douban->find('li.item');
+    foreach ($links as $key => $value)
+    {
+        echo "$value<br>";
+
+    }
 
 }
 
-
+//show_detail();
 
 
 
